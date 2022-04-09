7ecc15611338db3cd11e9d3eaa2afa63ba49bde0
elisacomposta <98694899+elisacomposta@users.noreply.github.com>
2022-04-09 15:09:13 +0200

    Init commit

--- run_qd.py
+++ run_qd.py
@@ -19,8 +19,4 @@ if __name__ == '__main__':
         experiment_name = experiment_name,
         args = args,
     )
-
-
-"""
-python run_qd.py --algo ppo --use-gae --lr 2.5e-4 --clip-param 0.1 --value-loss-coef 0.5 --num-processes 4 --num-steps 128 --num-mini-batch 4 --log-interval 100 --use-linear-lr-decay --entropy-coef 0.01 --eval-interval 20
-"""
+    
