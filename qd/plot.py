63b47bfedefe213b2e4fc035bb4bb574ce84298f
elisacomposta <98694899+elisacomposta@users.noreply.github.com>
2022-04-23 16:46:24 +0200

    set ticks

--- qd/plot.py
+++ qd/plot.py
@@ -8,14 +8,21 @@ def plotTrend(best_fitness, fileName, fitnessDomain = None):
     plt.xlabel("generation", fontsize = 16)
     plt.ylabel("fitness", fontsize = 16)
 
-    plt.xticks(x)
-    
+    if len(x) < 10:
+        xticks = x
+    else:
+        nb_xticks = 12
+        stepx = np.round(x[-1] / nb_xticks, 0)
+        xticks = np.arange(0, stepx * nb_xticks + 2*stepx, stepx)
+
     if fitnessDomain is None:
-        plt.yticks(np.arange(fitnessDomain[0], fitnessDomain[1]).round(2))
+        yticks = np.arange(fitnessDomain[0], fitnessDomain[1]).round(2)
     else:
         stepy = ( y[-1] - y[0] ) / 7
-        plt.yticks( np.arange(y[0], y[-1] + stepy, stepy).round(2) )
+        yticks = np.arange(y[0], y[-1] + stepy, stepy).round(2)
     
+    plt.xticks(xticks)
+    plt.yticks(yticks)
 
     plt.grid(which='major', color=(0.8,0.8,0.8,0.5), linestyle='-', linewidth=0.1)
 
